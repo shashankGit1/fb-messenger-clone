f52f4ee6934ec6a19657921a043fadc0685fa1b6
shashankGit1 <1806421@kiit.ac.in>
2021-07-03 23:54:47 +0530

    Nominal Restructuring

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Card, CardActions, CardContent, Typography } from "@material-ui/core";
 import "./Message.css";
-function Message(msgProp, username) {
-  const isUser = username === msgProp.username;
+
+function Message({message, username}) {
+  const isUser = username === message.username;
 
   return (
-    <div>
-      <Card className={`message $(isUser && 'message__user`}>
+    <div className= {`message ${isUser && 'message__user'}`}>
+      <Card className={ isUser ? "message__userCard" : "message__guestCard"}>
         <CardContent>
           <Typography color="pink" variant="h5" component="h2">
-            {msgProp.username}:{msgProp.text}
+            {message.username}:{message.text}
           </Typography>
         </CardContent>
       </Card>
